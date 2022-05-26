a0e4419da32ab09fc6db735cb06a699977fa3d2d
darlingzero0216 <darlingzero0216@gmail.com>
2022-05-26 10:37:33 -0500

    Proyectos

---
+++ Estructuras_de_datos/Tercer_corte/Cadenas/trimEnd()/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace trimEnd
+{
+    class trimEnd
+    {
+        static void Main(string[] args)
+        {
+                string sentence = "Hola gente, como estan el dia de hoy.";
+                char[] charsToTrim = {',', '.', ' '};
+                string[] words = sentence.Split();
+                foreach (string word in words)
+                Console.WriteLine(word.TrimEnd(charsToTrim));
+        }
+    }
+}
